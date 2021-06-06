b1d3fc9b0c5ca03b78a05d8cc4d2b0e88c942dd5
Шумилин Иван <mirovengil@gmail.com>
2021-06-06 16:53:12 +0300

    Пока ведутся работы, не могу ничего прокомментировать.

---
+++ game_object.py
@@ -0,0 +1,28 @@
+'''
+Модуль реализует работу игровых объектов.
+Он содержит класс объекта и методы для его обработки.
+'''
+
+FEATURES = {
+    "zombie" : {
+        'is_visible' : True,
+        'hp' : 15,
+        'sight_len' : 10,
+    },
+}
+
+class GameObject:
+    '''
+    Класс игрового объекта.
+    coords : (int, int) -- координаты на карте.
+    name : str -- название объекта.
+    is_visible : bool -- истина, если при попадании в поле зрения объект отображается (для
+    реализации всевозможных ловушек/невидимок/триггеров).
+    hp : int -- здоровье объекта (у построек, соответственно, -- прочность).
+    sight_len : int -- радиус зрения (подробнее можно узнать в ./docs/sight.png. 
+    '''
+    def __init__(self, name, coords):
+        self.name = name
+        self.coords = coords
+        self.is_visible = FEATURES[name]['is_visible']
+        self.hp = FEATURES[name]['hp']
